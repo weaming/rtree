dfd5d09c051a75105d6478f2127ff471768e3d3d
weaming <garden.yuen@gmail.com>
2018-08-08 22:44:07 +0800

    finish cli

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
+extern crate clap;
 extern crate filetree;
+
+use clap::{App, Arg};
 
 use std::env;
 
 use filetree::{node, print};
 
 fn main() {
-    let path = ".";
-    let root = "/User";
+    let matches = App::new("rtree")
+        .version("1.0")
+        .author("weaming <garden.yuen@gmail.com>")
+        .about("simple tree implemented in Rust")
+        .arg(
+            Arg::with_name("raw")
+                .long("raw")
+                .help("print raw size in bytes"),
+        ).arg(
+            Arg::with_name("root")
+                .value_name("ROOT")
+                .short("r")
+                .long("root")
+                .help("the root to get relative path of tree data structure"),
+        ).arg(
+            Arg::with_name("path")
+                .value_name("PATH")
+                .help("the directory to be list"),
+        ).get_matches();
+    let path = matches.value_of("path").unwrap_or(".");
+    let root = matches.value_of("root").unwrap_or(&path);
+    let human = !matches.is_present("raw");
     let level = 100;
-    let human = true;
+
     let root_node = node::new_file_node(&String::from(path), &String::from(root)).unwrap();
 
     match env::var("INDENT_MODE") {
