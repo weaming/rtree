31675ae62d02860498e1312609e3dd5e296fd9af
weaming <garden.yuen@gmail.com>
2018-08-08 22:55:02 +0800

    add flag indent

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,11 @@ fn main() {
                 .long("raw")
                 .help("print raw size in bytes"),
         ).arg(
+            Arg::with_name("indent")
+                .short("i")
+                .long("indent")
+                .help("show structure using 4 spaces indent"),
+        ).arg(
             Arg::with_name("root")
                 .value_name("ROOT")
                 .short("r")
@@ -30,12 +35,21 @@ fn main() {
     let path = matches.value_of("path").unwrap_or(".");
     let root = matches.value_of("root").unwrap_or(&path);
     let human = !matches.is_present("raw");
+    let indent = matches.is_present("indent");
     let level = 100;
 
     let root_node = node::new_file_node(&String::from(path), &String::from(root)).unwrap();
 
-    match env::var("INDENT_MODE") {
-        Ok(_) => print::print_file_node_simplely(&root_node, 1, level, human),
-        Err(_) => print::print_file_node_tree(&root_node, &mut vec![], 1, level, human),
+    match env::var("DEBUG") {
+        Ok(_) => {
+            println!("path {}", path);
+            println!("root {}", root);
+            println!("human {}", human);
+        }
+        Err(_) => {}
+    }
+    match indent {
+        true => print::print_file_node_simplely(&root_node, 1, level, human),
+        false => print::print_file_node_tree(&root_node, &mut vec![], 1, level, human),
     }
 }
